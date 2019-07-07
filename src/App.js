85913522945e76f4e0630d83ee34837636103d69
ladyleet <ladyleet@ladyleets-MacBook-Pro.local>
2019-07-07 11:13:22 -0400

    complete setup and reconfigure router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import AppRouter from './router';
 import './App.css';
+import Log from './Log';
+import Feed from './Feed';
+import BbqTips from './BbqTips';
+import Home from './Home';
+import LoginSignup from './LoginSignup';
+import Newsletter from './Newsletter';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <AppRouter/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="Router-Styling">
+          <span className="App-Routes">
+              <Link to="/">Home</Link>
+          </span>
+          <span className="App-Routes">
+              <Link to="/feed/">Feed</Link>
+          </span>
+          <span className="App-Routes">
+              <Link to="/log/">Log</Link>
+          </span>
+          <span className="App-Routes">
+              <Link to="/bbqtips/">BBQ Tips</Link>
+          </span>
+          <span className="App-Routes">
+              <Link to="/login_signup/">Login/Signup</Link>
+          </span>
+          <span className="App-Routes">
+              <a href="https://mailchi.mp/7fd00f8f4f23/meatdevs">Newsletter</a>
+          </span>
+        </div>
+        <div className="App-Title">
+          MEAT DEVS
+        </div>
+        <div>
+          <Route path="/" exact component={Home} />
+          <Route path="/feed/" component={Feed} />
+          <Route path="/log/" component={Log} />
+          <Route path="/bbqtips/" component={BbqTips} />
+          <Route path="/login_signup/" component={LoginSignup} />
+        </div>
+      </Router>
     </div>
   );
 }
