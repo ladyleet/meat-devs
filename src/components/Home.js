2575ce43dffbe383d928fad94b0b9f3dc1fce56e
Brandon M <development@brandonmcginnis.com>
2019-08-24 06:21:09 -0400

    Changed to ReactFragment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <>
+        <React.Fragment>
             <h2>Welcome to Meat Devs!</h2>
             <p>Where the swine and bovine loving developers congregate.</p>
             <p>Take a look around and <Link to='/contact/'>tell us</Link> what you think</p>
-        </>
+        </React.Fragment>
     );
 }
 
